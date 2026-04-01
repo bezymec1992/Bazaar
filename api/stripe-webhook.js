6ac554347eda42a7dbeb193e09676bca7e214631
VasylLavrov <bbezzymecc@gmail.com>
2026-04-01 23:06:30 +0100

    add resend

--- api/stripe-webhook.js
+++ api/stripe-webhook.js
@@ -1,6 +1,8 @@
 import Stripe from 'stripe';
+import { Resend } from 'resend';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const config = {
   api: {
