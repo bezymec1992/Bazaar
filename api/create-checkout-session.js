1355cc5a726986d08a088d26003fec3b2ca89d33
VasylLavrov <bbezzymecc@gmail.com>
2026-04-01 23:34:36 +0100

    fix template

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -86,6 +86,7 @@ export default async function handler(req, res) {
         productId: product.id,
         title: product.title,
         price: product.price,
+        image: product.image,
       },
       line_items: [
         {
--- api/stripe-webhook.js
+++ api/stripe-webhook.js
@@ -43,10 +43,48 @@ export default async function handler(req, res) {
         to: 'bbezzymecc@gmail.com', // ← замени на свой
         subject: 'Test email',
         html: `
-  <h2>New Order 🎉</h2>
-  <p><strong>Product:</strong> ${metadata.title}</p>
-  <p><strong>Price:</strong> €${metadata.price}</p>
-  <p><strong>ID:</strong> ${metadata.productId}</p>
+<div style="margin: 0; padding: 0; color: #d4af37; font-family: Arial, sans-serif;">
+  <div style="max-width: 600px; margin: 40px auto; padding: 30px; background: #0f4a1a; border-radius: 12px; box-shadow: 0 0 20px rgba(0,0,0,0.3);">
+    <div style="text-align:center; margin-bottom:15px;">
+      <img 
+        src="https://bezymec1992.github.io/Bazaar/imgs/logo.png"
+        width="70"
+        style="display:block; margin:0 auto;"
+      />
+    </div>
+    <h2 style="margin-top: 0; text-align: center; color: #d4af37; letter-spacing: 1px;">
+      New Order from Bazaar
+    </h2>
+
+    <hr style="border: none; border-top: 1px solid rgba(212,175,55,0.3); margin: 20px 0;">
+
+    <p style="font-size: 14px; opacity: 0.8;">
+      You have received a new order from your website:
+    </p>
+
+    <div style="margin-top: 20px; padding: 15px; background: #0b3a14; border-radius: 8px;">
+      
+      <h2 style="margin-bottom:20px;">🛒 New Order</h2>
+
+      <p><strong>Product:</strong> ${metadata.title}</p>
+      <p><strong>Price:</strong> €${metadata.price}</p>
+      <p><strong>ID:</strong> ${metadata.productId}</p>
+
+      <img src="${metadata.image}" width="150" style="margin-top:10px; border-radius:6px;" />
+
+      <hr style="margin:20px 0;">
+
+      <p style="color:#888;">Order from Bazaar website</p>
+    </div>
+
+    <hr style="border: none; border-top: 1px solid rgba(212,175,55,0.3); margin: 25px 0;">
+
+    <p style="text-align: center; font-size: 12px; opacity: 0.7;">
+      Bazaar Antiques • Website Order Notification
+    </p>
+
+  </div>
+</div>
 `,
       });
 
