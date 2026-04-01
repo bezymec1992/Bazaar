bc3001618c5718a07a9c7adf86fd195b0a75c0db
VasylLavrov <bbezzymecc@gmail.com>
2026-04-01 22:19:10 +0100

    fix

--- api/stripe-webhook.js
+++ api/stripe-webhook.js
@@ -1,5 +1,4 @@
 import Stripe from 'stripe';
-import { buffer } from 'micro';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET);
 
@@ -10,26 +9,28 @@ export const config = {
 };
 
 export default async function handler(req, res) {
+  let buf = '';
+
+  for await (const chunk of req) {
+    buf += chunk;
+  }
+
   const sig = req.headers['stripe-signature'];
 
   let event;
 
   try {
-    const buf = await buffer(req);
-
     event = stripe.webhooks.constructEvent(buf, sig, process.env.STRIPE_WEBHOOK_SECRET);
   } catch (err) {
     console.error('Webhook error:', err.message);
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
 
-  // ✅ если оплата успешна
+  // ✅ УСПЕШНАЯ ОПЛАТА
   if (event.type === 'checkout.session.completed') {
     const session = event.data.object;
 
     console.log('Payment successful:', session.id);
-
-    // 👉 пока просто лог
   }
 
   res.status(200).json({ received: true });
