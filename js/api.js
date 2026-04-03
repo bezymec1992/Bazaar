44891a303febe34b1220e3b3a1c4e389aa02fe11
VasylLavrov <bbezzymecc@gmail.com>
2026-04-03 21:13:51 +0100

    fix supabase

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = 'https://oicwhdcmfkckprrnzctn.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_YebYXtFgqG3G0sGJH8VAUA_G-ZrlSL-';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // const API_URL = 'https://sheetdb.io/api/v1/jqeo93r4g20ic';
 
@@ -11,7 +11,7 @@ const CACHE_VERSION = 'v2';
 const CACHE_TIME = 1000 * 60 * 20; // 20 минут
 
 async function getProducts() {
-  const { data, error } = await supabase.from('products').select('*');
+  const { data, error } = await db.from('products').select('*');
 
   if (error) {
     console.error('Supabase error:', error);
